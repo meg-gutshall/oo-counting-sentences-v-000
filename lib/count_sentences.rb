a847a39d8c82909d993aa101f0210937415baf41
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-02 21:00:44 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,10 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    self.split(/[\.?!]/).count {|sentence| sentence != ""}
   end
-
-  # def count_sentences
-  #   self.split(/[\.?!]/).count {|sentence| sentence != ""}
-  # end
 end
